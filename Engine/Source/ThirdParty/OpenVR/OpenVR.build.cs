7e48df94eb861830f98bf3976daa6c295ecb6e50
Jules Blok <jules.blok@gmail.com>
2017-03-06 02:55:12 +0100

    Initial proof-of-concept.

---
+++ Engine/Source/ThirdParty/OpenVR/OpenVR.build.cs
@@ -0,0 +1,55 @@
+﻿// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+using System;
+using System.IO;
+using UnrealBuildTool;
+
+public class OpenVR : ModuleRules
+{
+	public OpenVR(TargetInfo Target)
+	{
+		/** Mark the current version of the OpenVR SDK */
+		string OpenVRVersion = "v1_0_2";
+		Type = ModuleType.External;
+
+		string SdkBase = UEBuildConfiguration.UEThirdPartySourceDirectory + "OpenVR/OpenVR" + OpenVRVersion;
+		if (!Directory.Exists(SdkBase))
+		{
+			string Err = string.Format("OpenVR SDK not found in {0}", SdkBase);
+			System.Console.WriteLine(Err);
+			throw new BuildException(Err);
+		}
+
+		PublicIncludePaths.Add(SdkBase + "/headers");
+
+		string LibraryPath = SdkBase + "/lib/";
+
+		if(Target.Platform == UnrealTargetPlatform.Win32)
+		{
+			PublicLibraryPaths.Add(LibraryPath + "win32");
+			PublicAdditionalLibraries.Add("openvr_api.lib");
+			PublicDelayLoadDLLs.Add("openvr_api.dll");
+
+			string OpenVRBinariesDir = String.Format("$(EngineDir)/Binaries/ThirdParty/OpenVR/OpenVR{0}/Win32/", OpenVRVersion);
+			RuntimeDependencies.Add(new RuntimeDependency(OpenVRBinariesDir + "openvr_api.dll"));
+		}
+		else if(Target.Platform == UnrealTargetPlatform.Win64)
+		{
+			PublicLibraryPaths.Add(LibraryPath + "win64");
+			PublicAdditionalLibraries.Add("openvr_api.lib");
+			PublicDelayLoadDLLs.Add("openvr_api.dll");
+
+			string OpenVRBinariesDir = String.Format("$(EngineDir)/Binaries/ThirdParty/OpenVR/OpenVR{0}/Win64/", OpenVRVersion);
+			RuntimeDependencies.Add(new RuntimeDependency(OpenVRBinariesDir + "openvr_api.dll"));
+		}
+		else if (Target.Platform == UnrealTargetPlatform.Mac)
+		{
+			string DylibPath = SdkBase + "/bin/osx32/libopenvr_api.dylib";
+			PublicDelayLoadDLLs.Add(DylibPath);
+			PublicAdditionalShadowFiles.Add(DylibPath);
+
+			string OpenVRBinariesDir = String.Format("$(EngineDir)/Binaries/ThirdParty/OpenVR/OpenVR{0}/osx32/", OpenVRVersion);
+			RuntimeDependencies.Add(new RuntimeDependency(OpenVRBinariesDir + "libopenvr_api.dylib"));
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# RoboRevive
+
+This is proof-of-concept Robo Recall mod that adds native Vive support. It's called RoboRevive, but it doesn't actually use the Revive compatibility layer. Instead it uses the native SteamVR plugin from the Unreal Engine.
+
+Contributions to improve the control scheme and other kinds of Vive customizations are more than welcome. I'd like to see people use this as a base to create a completely native version of Robo Recall.
+
+# Installation
+
+## Oculus Home
+
+1. Download Robo Recall from Oculus Home.
+2. Download OpenVR.zip and Revive.robo from the [releases page](https://github.com/LibreVR/RoboRevive/releases).
+3. Go to the directory where Robo Recall is installed, by default this is `C:\Program Files\Oculus\Software\Software\epic-games-odin`.
+4. Extract the `Engine` folder from OpenVR.zip in this directory, make sure you merge it with the existing folder.
+5. Go to `RoboRecall\Binaries\Win64` and drag Revive.robo onto the `RoboRecallModInstaller.exe` (or just double-click Revive.robo if it's already associated).
+6. Press 'Yes' to install the mod.
+7. Click 'Play Now' after installation or just run `RoboRecall-Win64-Shipping.exe` directly.
+
+## Robo Recall Editor
+
+1. Download the Robo Recall Editor through the Epic Games Launcher.
+2. [Download this repository](https://github.com/LibreVR/RoboRevive/archive/master.zip) as a zip file.
+3. Go to the directory where the Robo Recall Editor is installed, by default this is `C:\Program Files\Epic Games\RoboRecallModKit\RoboRecall`.
+4. Extract the contents of the zip file into the directory, be sure to merge it with the existing folders.
+5. Start the Robo Recall Editor, it may ask to rebuild a few modules, click 'Yes' to recompile them.
+6. Now go add Vive Controller render models and submit your contribution as a Pull Request.
